51e90b080c4e77f9ae31b48937daf3abb5233e10
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-09-22 17:11:52 +0500

    Authentication Implemented

--- src/components/commons/LoginInput.js
+++ src/components/commons/LoginInput.js
@@ -1,7 +1,7 @@
-const LoginInput = ({ ...rest }) => {
+const LoginInput = ({ classes, ...rest }) => {
   return (
     <input
-      className="rounded-md w-full outline-none bg-[#F5F6FA] border border-[#E7E7E7] p-2"
+      className={`rounded-md w-full outline-none bg-[#F5F6FA] border border-[#E7E7E7] p-2 ${classes}`}
       {...rest}
     />
   );
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,35 +1,103 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useMutation } from "@apollo/client";
-import { LoginInput } from "../components";
-import { useState } from "react";
 import { LOGIN_USER } from "../graphQl/Mutation";
+import { LoginInput } from "../components";
+
+function setIem(key, value) {
+  // `item` is an object which contains the original value
+  // as well as the time when it's supposed to expire
+  const item = {
+    value: value,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+function getItem(key) {
+  const itemStr = localStorage.getItem(key);
+  // if the item doesn't exist, return null
+  if (!itemStr) {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  // compare the expiry time of the item with the current time
+  if (now.getTime() > item.expiry) {
+    // If the item is expired, delete the item from storage
+    // and return null
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+}
 
 const LogIn = () => {
-  const [login, { error} ] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // const [checked, setChecked] = useState < boolean > true;
+
+  const [showErrors, setShowErrors] = useState(false);
 
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    rememberMe: false,
   });
+
+  useEffect(() => {
+    const response = getItem("credentials");
+    if (response) {
+      const { email, password } = response;
+      setFormData({
+        email,
+        password,
+        rememberMe: true,
+      });
+    } else {
+      console.log(response);
+    }
+  }, []);
+
+
+  const changeHandler = async (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-try {
-const resposnce = await login({
-    variables: {
-      email: "mahmudakash177@gmail.com",
-      password: "123456",
-    },
-  });
-    console.log(error);
-    console.log(resposnce);
-} catch (error) {
-  console.log(error.message)
-}
-
-
-  
+    if (formData.email && formData.password) {
+      setShowErrors(false);
+      try {
+        const { email, password, rememberMe } = formData;
+        const { data } = await login({
+          variables: {
+            email,
+            password,
+          },
+        });
+        // Login SuccessFull
+        if (data.login) {
+          console.log("login successfull");
+          // If remeber me is clicked then store the credentials
+          if (rememberMe) {
+            setIem("credentials", {
+              email,
+              password,
+            });
+          }
+        } else {
+          // Login Failure
+          console.log("login fail");
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      setShowErrors(true);
+    }
   };
 
   return (
@@ -44,12 +112,15 @@ const resposnce = await login({
           <h1 className="font-semibold my-3 md:my-5 text-2xl text-[#0A111F]">
             Sign in to your Account
           </h1>
-          <form className="space-y-2.5 mt-4 md:mt-10">
+          <form onSubmit={submitHandler} className="space-y-2.5 mt-4 md:mt-10">
             <div className="flex flex-col space-y-2">
               <label className="text-[#0A111F] font-medium text-base">
                 Email address
               </label>
               <LoginInput
+                value={formData.email}
+                classes={showErrors && formData.email === "" && "border-[red]"}
+                onChange={changeHandler}
                 placeholder="example@gmail.com"
                 name="email"
                 type="email"
@@ -60,21 +131,37 @@ const resposnce = await login({
                 Password
               </label>
               <LoginInput
+                classes={
+                  showErrors && formData.password === "" && "border-[red]"
+                }
+                value={formData.password}
+                onChange={changeHandler}
                 placeholder="........."
-                name="email"
+                name="password"
                 type="password"
               />
             </div>
             <div className="flex items-center justify-between font-normal text-xs text-[#0A111F]">
               <div className="flex items-center space-x-1">
-                <input type={"checkbox"} />
-                <label>Remember me?</label>
+                <input
+                  id="rememberme"
+                  type={"checkbox"}
+                  checked={formData.rememberMe}
+                  onChange={() => {
+                    setFormData({
+                      ...formData,
+                      rememberMe: !formData.rememberMe,
+                    });
+                  }}
+                />
+                <label htmlFor="rememberme">Remember me?</label>
               </div>
               <h5 className="underline">forgot password</h5>
             </div>
 
             <button
               onClick={submitHandler}
+              type="submit"
               className="rounded-md block text-center text-[#ffffff] text-base font-semibold bg-[#D85C27] w-full py-2"
             >
               Log In
