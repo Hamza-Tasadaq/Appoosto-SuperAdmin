62dcb8cb7c47a306f58cca8064cb2f88fdfc54a2
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-09-24 15:04:24 +0500

    Loader Added

--- src/components/commons/Loader.js
+++ src/components/commons/Loader.js
@@ -1,5 +1,11 @@
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center space-x-8">
+      <div className="animate-ping h-8 w-8 rounded-full bg-[#D85C27]" />
+      <div className="animate-ping h-8 w-8 rounded-full bg-[#D85C27]" />
+      <div className="animate-ping h-8 w-8 rounded-full bg-[#D85C27]" />
+    </div>
+  );
 };
 
 export default Loader;
