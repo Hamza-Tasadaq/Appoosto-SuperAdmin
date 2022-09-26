cc3d81c8e17c2dfb502b59d805496d028de3dddf
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-09-27 01:23:42 +0500

    Resturant Structure Added

--- src/pages/Resturants.js
+++ src/pages/Resturants.js
@@ -1,5 +1,17 @@
 const Resturants = () => {
-  return <div>Resturants</div>;
+  return (
+    <div className="bg-[#FFFFFF] rounded-lg px-5 py-6 boxShadow">
+      <div></div>
+
+      <div className="bg-[#EFF3F7] rounded-lg px-4 py-4 space-y-3">
+        <div className="bg-[#FFFFFF] rounded-lg px-4 py-4">h</div>
+        <div className="bg-[#FFFFFF] rounded-lg px-4 py-4">h</div>
+        <div className="bg-[#FFFFFF] rounded-lg px-4 py-4">h</div>
+        <div className="bg-[#FFFFFF] rounded-lg px-4 py-4">h</div>
+        <div className="bg-[#FFFFFF] rounded-lg px-4 py-4">h</div>
+      </div>
+    </div>
+  );
 };
 
 export default Resturants;
