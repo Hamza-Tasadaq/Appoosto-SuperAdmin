eae452785a670dfa4f7c7c4e21ab33ca025fd601
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-09-27 23:57:18 +0500

    Schema Updated

--- src/graphQl/mutations/LoginMutation.js
+++ src/graphQl/mutations/LoginMutation.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 // Login User
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String, $password: String) {
+  mutation loginUser($email: String!, $password: String!) {
     login(user: { email: $email, password: $password })
   }
 `;
